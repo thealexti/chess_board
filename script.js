aaad1b7a39b1bf34942c93f9c679dd0019287283
thealexti <thealexti@users.noreply.github.com>
2020-06-02 22:33:38 +0300

    using function

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
-var x=8;
-var y=8;
+function createChessBoard() {
 
-var chessBoard = document.getElementById("chessBoard");
-
-for (var i=0; i<y; i++){
-    var row = chessBoard.appendChild(document.createElement("div"));
-    for (var j=0; j<x; j++){
-        row.appendChild(document.createElement("span"));
+    const x=8;
+    const y=8;
+    
+    const chessBoard = document.getElementById("chessBoard");
+    
+    for (let i=0; i<y; i++){
+        const row = chessBoard.appendChild(document.createElement("div"));
+        for (let j=0; j<x; j++){
+            row.appendChild(document.createElement("span"));
+        }  
     }
 }
+    window.onload = createChessBoard;"\n"
